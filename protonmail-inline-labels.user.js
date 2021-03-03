96f35d19dd6605fa6158e428e1f978157c60e18f
Scott Werner <scott.werner.vt@gmail.com>
2021-03-03 18:02:13 -0500

    Update userscript doc string.

--- protonmail-inline-labels.user.js
+++ protonmail-inline-labels.user.js
@@ -1,15 +1,24 @@
 // ==UserScript==
 // @name        ProtonMail - Inline Labels
-// @namespace   Violentmonkey Scripts
-// @version     2020.02.15
+// @version     2020.03.03
+// @description Toggle labels from stack to inline.
+// @license     MIT
+// @author      Scott Werner <scott.werner.vt@protonmail.com>
+// @namespace   https://github.com/scottwernervt
+// @include     https://github.com/*
+// @include     https://gist.github.com/*
+// @run-at      document-idle
+// @grant       none
 // @match       https://mail.protonmail.com/*
 // @match       https://beta.protonmail.com/*
-// @grant       none
-// @run-at      document-end
 // @require     https://code.jquery.com/jquery-3.5.1.min.js
 // @require     https://gist.github.com/BrockA/2625891/raw/waitForKeyElements.js
-// @author      Scott Werner <scott.werner.vt@protonmail.com>
-// @description Toggle labels from stack to inline.
+// @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?updated=20180103
+// @require     https://greasyfork.org/scripts/28721-mutations/code/mutations.js?version=882023
+// @icon        https://mail.protonmail.com/assets/favicons/apple-touch-icon-180x180.png
+// @updateURL   https://raw.githubusercontent.com/scottwernervt/userscripts/master/protonmail-inline-labels.user.js
+// @downloadURL https://raw.githubusercontent.com/scottwernervt/userscripts/master/protonmail-inline-labels.user.js
+// @supportURL  https://github.com/scottwernervt/userscripts/issues
 // ==/UserScript==
 
 function setInlineLabels(elements) {
