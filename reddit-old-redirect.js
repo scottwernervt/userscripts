68989e20760308afaa672af0521d730448367011
Scott Werner <scott.werner.vt@protonmail.com>
2023-06-29 09:28:54 -0400

    Fix UserScript header.

--- reddit-old-redirect.js
+++ reddit-old-redirect.js
@@ -2,14 +2,12 @@
 // @name        Reddit Old Redirect
 // @author      Scott Werner <scott.werner.vt@protonmail.com>
 // @namespace   https://github.com/scottwernervt
-// @include     https://github.com/*
-// @include     https://gist.github.com/*
+// @version     2023.06.29
+// @description Automatically redirect from Reddit Redesign to Reddit Old UI.
+// @icon        https://www.redditstatic.com/shreddit/assets/favicon/180x180.png
 // @match       https://www.reddit.com/*
 // @grant       none
 // @run-at      document-idle
-// @version     2023.06.29
-// @icon        https://www.redditstatic.com/shreddit/assets/favicon/180x180.png
-// @description Automatically redirect from Reddit Redesign to Reddit Old UI.
 // ==/UserScript==
 
 (function() {
