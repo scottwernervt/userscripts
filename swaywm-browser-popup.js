a7117e81e6ea5d2f8f6a1b492e0abab73e2fc438
Scott Werner <scott.werner.vt@protonmail.com>
2023-11-13 09:23:14 -0500

    Do not check if menubar is visible.

--- swaywm-browser-popup.js
+++ swaywm-browser-popup.js
@@ -4,14 +4,15 @@
 // @version      0.1
 // @description  Detect "pop-up" windows and float them in swaywm. Test with
 // https://webbrowsertools.com/popup-blocker/
-// @author       Maddison Hellstrom <github.com/b0o>
+// @author       Scott Werner
 // @include      /^.*$/
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 (function() {
-    if ((window.hasOwnProperty("titlebar") && window.titlebar.visible === false)
-        || (window.menubar && window.menubar.visible === false)) {
+    const isTitlebarVisible = window.hasOwnProperty("titlebar") && window.titlebar.visible;
+    // const isMenubarVisible = window.menubar && window.menubar.visible === false;
+    if (isTitlebarVisible) {
         document.title = `<-POPUP-> ${document.title}`;
     }
 })()
