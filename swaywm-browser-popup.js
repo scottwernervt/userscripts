c57ef7162e26ee3719cbee138bc65b4882ff31ff
Scott Werner <scott.werner.vt@protonmail.com>
2023-11-13 19:57:25 -0500

    Refactor variables and checks.

--- swaywm-browser-popup.js
+++ swaywm-browser-popup.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         Sway Popup Window Detector
 // @namespace    https://gist.github.com/b0o/f00de53aa44ff53b39f997d83b7b5585
-// @version      0.1
+// @version      0.2
 // @description  Detect "pop-up" windows and float them in swaywm. Test with
 // https://webbrowsertools.com/popup-blocker/
 // @author       Scott Werner
 // @include      /^.*$/
 // @grant        none
-// @run-at       document-start
+// @run-at document-end
 // ==/UserScript==
 (function() {
-    const isTitlebarVisible = window.hasOwnProperty("titlebar") && window.titlebar.visible;
-    // const isMenubarVisible = window.menubar && window.menubar.visible === false;
-    if (isTitlebarVisible) {
+    const isTitlebarHidden = window.hasOwnProperty("titlebar") && window.titlebar.visible === false;
+    const isMenubarHidden = window.menubar && window.menubar.visible === false;
+    if (isTitlebarHidden || isMenubarHidden) {
         document.title = `<-POPUP-> ${document.title}`;
     }
 })();
